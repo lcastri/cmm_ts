30e3b014e6824297c65b7fae643c5f208f98e274
Luca Castri <lucacastri94@gmail.com>
2022-10-31 10:25:20 +0000

    fixexs

--- load.py
+++ load.py
@@ -1,11 +1,22 @@
 from Data import Data
 from constants import RESULT_DIR
-from models.mIAED import mIAED
+# IAED import
+from models.IAED.mIAED import mIAED
+from models.IAED.sIAED import sIAED
+from models.IAED.config import config as cIAED
+# T2V import
+from models.T2V.mT2VRNN import mT2VRNN
+from models.T2V.sT2VRNN import sT2VRNN
+from models.T2V.config import CONFIG as cT2V
+# CNN import
+from models.CNNLSTM.mCNNLSTM import mCNNLSTM
+from models.CNNLSTM.sCNNLSTM import sCNNLSTM
+from models.CNNLSTM.config import CONFIG as cCNN
 from models.utils import get_df
 
 
-N_FUTURE = 150
-N_PAST = 20
+N_FUTURE = 48
+N_PAST = 32
 N_DELAY = 0
 TRAIN_PERC = 0.0
 VAL_PERC = 0.0
@@ -13,31 +24,20 @@ TEST_PERC = 1.0
 
 
 TEST_AGENTS = [3, 4, 5, 6, 7, 8, 9, 10]
-MODELS = ["mIAED", "mIAED_att", "FPCMCI_mIAED_catt_tc", "PCMCI_mIAED_catt_tc"]
+MODELS = ["256_mIAED_FPCMCI_t005", "256_mIAED_PCMCI_t005", "mIAED_FPCMCI_t01", "mIAED_PCMCI_t01"]
 for M in MODELS:
-    m = mIAED(folder = M)
     for a in TEST_AGENTS:
         df, features = get_df(a)
+        m = mT2VRNN(df = df, folder = M)
         d = Data(df, N_PAST, N_DELAY, N_FUTURE, TRAIN_PERC, VAL_PERC, TEST_PERC)
-        d.augment()
         d.downsample(10)
-        X_train, y_train, X_val, y_val, X_test, y_test = d.get_timeseries()
+        d.smooth(window_size = 50)
+        _, _, _, _, X_test, y_test = d.get_timeseries()
 
         # Model evaluation
-        m.RMSE(X_test, y_test, d.scaler, folder = RESULT_DIR + "/" + M + '/test/' + str(a))
-        # Model predictions
-        m.predict(X_test, y_test, d.scaler, folder = RESULT_DIR + "/" + M + '/prediction/' + str(a))
+        m.MAE(X_test, y_test, d.scaler, folder = RESULT_DIR + "/" + M + '/test/' + str(a))
+        
+        # # Model predictions
+        # m.predict(X_test, y_test, d.scaler, folder = RESULT_DIR + "/" + M + '/prediction/' + str(a))
 
     
-
-
-
-# m.mean_RMSE(np.load(m.model_dir +  "/rmse.npy"))
-
-# model.save_cmatrix()
-
-# # Model evaluation
-# model.RMSE(x_test, y_test, d.scaler)
-
-# # Model predictionsmk dir 
-# model.plot_predictions(x_test, y_test, d.scaler)
